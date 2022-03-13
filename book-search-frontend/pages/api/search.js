5dd577d5be0b4786db60c9dbc2f63580882964f4
dennis0126 <dennis940126@gmail.com>
2022-03-13 22:34:38 +0800

    redirect to book page when search keyword is isbn13

--- book-search-frontend/pages/api/search.js
+++ book-search-frontend/pages/api/search.js
@@ -3,6 +3,12 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
     const searchKeyword = req.body.searchKeyword;
+
+    // redirect to book page when search keyword is isbn13
+    if (/^[0-9]{13}$/.test(searchKeyword)) {
+      res.redirect(`/book/${searchKeyword}`);
+    }
+
     res.redirect(`/?search=${encodeURI(searchKeyword)}`);
   }
 }
